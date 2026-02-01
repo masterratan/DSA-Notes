6c87f1f794951e36519fe9b767af7f3989b0f978
masterratan <itsratan.choudhary@gmail.com>
2026-02-02 03:49:57 +0530

    added snippets

---
+++ Snippet/init.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define F first
+#define S second
+#define MP make_pair
+#define pb push_back
+#define ppb pop_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+const int MOD = 1e9+7;
+const int INF = 1e9;
+const long long LINF = 1e18;
+const long double PI = acos(-1.0L);
+
+int d[] = {0,1,0,-1,0};
+int dx[] = {0,1,1,1,0,-1,-1,-1};
+int dy[] = {1,1,0,-1,-1,-1,0,1};
+
+void solve() {
+    
+
+    cout<<'\n';
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
