4e32f030fd1969ab13a851bc34dbca5562d261af
masterratan <itsratan.choudhary@gmail.com>
2026-02-02 04:11:37 +0530

    refactor code

--- Graph/DSU by Rank & Path Compression.cpp
+++ Graph/DSU by Rank & Path Compression.cpp
@@ -1,24 +1,17 @@
 vector<int> parent;
 vector<int> rank;
-int find (int x) {
-    if (x == parent[x]) 
-        return x;
-
-    return parent[x] = find(parent[x]);
+int find(int i) {
+    if(i == parent[i]) return i;
+    return parent[i] = find(parent[i]);
 }
-void Union (int x, int y) {
+void Union(int x, int y) {
     int x_parent = find(x);
     int y_parent = find(y);
-
-    if (x_parent == y_parent) 
-        return;
-
-    if(rank[x_parent] > rank[y_parent]) {
+    if(x_parent == y_parent) return;
+    if(rank[x_parent] > rank[y_parent]) parent[y_parent] = x_parent;
+    else if(rank[x_parent] < rank[y_parent]) parent[x_parent] = y_parent;
+    else {
         parent[y_parent] = x_parent;
-    } else if(rank[x_parent] < rank[y_parent]) {
-        parent[x_parent] = y_parent;
-    } else {
-        parent[x_parent] = y_parent;
-        rank[y_parent]++;
+        rank[x_parent]++;
     }
 }
