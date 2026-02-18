98a4ccf3212515a5275c2aa1c6ae6c517009484f
masterratan <itsratan.choudhary@gmail.com>
2026-02-18 23:13:09 +0530

    added stacks

--- Bit Manipulation/Bit Manipulation.cpp
+++ Bit Manipulation/Bit Manipulation.cpp
@@ -1,27 +1,27 @@
-Odd or even
+// Odd or even
 return a&1;
 
-Power of 2 or not
+// Power of 2 or not
 return N & (N-1) == 0;
 
-Swap a & b
+// Swap a & b
 a = a^b
 b = a^b
 a = a^b
 
-Set k-th bit
+// Set k-th bit
 N | (1<<k)
 
-Clear k-th bit
+// Clear k-th bit
 N & ~(1<<k)
 
-Toggle k-th bit
+// Toggle k-th bit
 N ^ (1<<k)
 
-Remove the last set bit (rightmost)
+// Remove the last set bit (rightmost)
 N & (N-1)
 
-Count Set Bits
+// Count Set Bits
 int cnt = 0;
 while(n > 1){
     cnt += n&1;
--- Miscellaneous/STL.cpp
+++ Miscellaneous/STL.cpp
@@ -1,14 +1,15 @@
-Memset() Function
-It is used to fill a block of memory with a particular value.
+// Memset() Function
+// It is used to fill a block of memory with a particular value.
 
 #include<cstring>
-memset(variable_name, values, sizeof(variable_name));
+// memset(variable_name, values, sizeof(variable_name));
 
 int arr[5] = {-1}
-Result: {-1,0,0,0,0}
+// Result: {-1,0,0,0,0}
 
 memset(arr, -1, sizeof(a)) 
-Result: {-1,-1,-1,-1,-1}
+// Result: {-1,-1,-1,-1,-1}
+_______________________________________________________________________________________________________________________
 
 // Note 'lower_bound' & 'upper_bound' functions returns iterators, not index or value
 
@@ -22,4 +23,21 @@ int idx = it - arr.begin();// index of lower bound
 auto it = upper_bound(arr.begin(), arr.end(), x);
 if(it == arr.end()) return -1;
 int val = *it; // value of upper bound
-int idx = it - arr.begin();// index of upper bound
+int idx = it - arr.begin(); // index of upper bound
+_______________________________________________________________________________________________________________________
+
+// Character to string
+string(1, ch) // means make a string of 'ch' character repeating 1 time
+
+// isAlphanumeric (0-9, a-z, A-Z) but not symbols like $, *, ^
+isalnum('a')  → true
+isalnum('Z')  → true
+isalnum('5')  → true
+isalnum('+')  → false
+isalnum('(')  → false
+
+// Maximum element in an array
+*max_element(arr.begin(), arr.end());
+
+// Minimum element in an array
+*min_element(arr.begin(), arr.end());
